a469e74e4b0c2f3c5410be13592087d2820e56b2
Enzo Teles <enzo.teles@finners.com.br>
2018-07-30 22:12:49 -0300

    criando a estrutura do projeto netshoes

---
+++ app/src/main/java/br/com/netshoes/MainActivity.kt
@@ -0,0 +1,20 @@
+package br.com.netshoes
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * Created by Enzo Teles on 30,July,2018
+ * Barueri - SP
+ * email: enzo.carvalho.teles@gmail.com
+ * Software Developer Sr.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/br/com/netshoes/about/AboutFragment.kt
@@ -0,0 +1,8 @@
+package br.com.netshoes.about
+
+/**
+ * Created by Enzo Teles on 30,July,2018
+ * Barueri - SP
+ * email: enzo.carvalho.teles@gmail.com
+ * Software Developer Sr.
+ */
